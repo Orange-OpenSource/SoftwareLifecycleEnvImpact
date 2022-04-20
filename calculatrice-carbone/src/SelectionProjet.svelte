fc73a2a1ed166625c3a8ecdc052997a50eb16397
Julie Duchateau <julie.duchateau.etu@univ-lille.fr>
2022-04-20 16:37:49 +0200

    Ajout arbre

--- calculatrice-carbone/src/SelectionProjet.svelte
+++ calculatrice-carbone/src/SelectionProjet.svelte
@@ -1,3 +1,54 @@
+<script>
+	import TreeView from './TreeView.svelte'
+	let children = [
+		{
+			name: "Project",
+			type: "type",
+            parent : "",
+			children: [
+				{
+					name: "Build",
+					type: "type",
+                    parent : "Project",
+                    children: [
+						{
+							name: "Management",
+							type: "type",
+							parent: "Project/Build"
+						},
+						{
+							name: "Specifications",
+							type: "type",
+							parent: "Project/Build"
+						},
+                        {
+							name: "Implementation",
+							type: "type",
+							parent: "Project/Build"
+						}
+					]
+				},
+				{
+					name: "Run",
+					type: "type",
+					children: [
+						{
+							name: "Maintenance",
+							type: "type",
+							parent: "Project/Run"
+						},
+						{
+							name: "Hosting",
+							type: "type",
+							parent: "Project/Run"
+						}
+					]
+				}
+			]
+		}
+	]
+</script>
+
 <div class="container">
     <div class="row h-100">
       <div class="col-3 border-right h-100">
@@ -11,6 +62,11 @@
       </div>
       <div class="col">
         <strong>Preview</strong>
+        
+        <div class="col">
+            <TreeView {children}></TreeView>
+        </div>
+
       </div>
     </div>
 </div>
---
+++ calculatrice-carbone/src/TreeView.svelte
@@ -0,0 +1,59 @@
+<script>
+    export let children  
+</script>
+  
+  {#each children as file}
+      <div class="tree">
+          <span>{file.name}</span>
+          {#if file.children}
+              <svelte:self children={file.children} />
+          {/if}
+ 
+      </div>
+   {/each}
+   <div class="tree"><button class="btn btn-primary">Add task</button></div>
+  
+  <style>
+    div.tree {
+        padding: 50px 0px 0px 96px;
+        position: relative;
+        text-align: start;
+
+        line-height:300%;
+    }
+
+    div.tree:before {
+        
+        content: "";
+        position: absolute;
+        top: 3px;
+        left: 15px;
+        border-left: 2px dashed #a2a5b5;
+        width: 1px;
+        height: 100%;
+    }
+
+    div.tree:after {
+        content: "";
+        position: absolute;
+        border-top: 2px dashed #a2a5b5;
+        top: 70px;
+        left: 20px;
+        width: 60px;
+    }
+
+    div.tree:last-child:before {
+        top: -22px;
+        height: 90px;
+    }
+
+    span {
+        background-color: #ececece1;
+        border-radius: 3px;
+        border : none;
+        padding : 10px  20px 10px 20px;
+        -webkit-box-shadow: 5px 5px 3px -6px #000000; 
+        box-shadow: 5px 5px 3px -6px #000000;
+    }
+    
+  </style>
