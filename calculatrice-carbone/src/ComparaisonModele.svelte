858848e96ee08952b438c82a72dea21b8ff6ead6
Julie Duchateau <julie.duchateau.etu@univ-lille.fr>
2022-04-21 09:45:28 +0200

    wip

---
+++ calculatrice-carbone/src/ComparaisonModele.svelte
@@ -0,0 +1,54 @@
+<div class="container">
+    <div class="row h-100">
+      <div class="col-3 border-right h-100">
+        <strong>Models :</strong>
+
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                <label class="form-check-label" for="defaultCheck1">
+                  First variant
+                </label>
+              </div></li>
+            <li class="list-group-item">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                <label class="form-check-label" for="defaultCheck1">
+                  Second variant
+                </label>
+              </div></li>
+            <li class="list-group-item">
+                <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                <label class="form-check-label" for="defaultCheck1">
+                  Third variant
+                </label>
+              </div>
+            </li>
+          </ul>
+      </div>
+      <div class="col">
+        <strong>Preview</strong>
+        
+        <div class="col">
+            
+        </div>
+
+      </div>
+    </div>
+</div>
+
+<style>
+
+    .container{
+        max-width : 100%;
+        height: 90%;
+        max-height: 90%;
+    }
+
+    .border-right {
+        border-right: 1px solid #ddd;
+    }
+
+</style>
---
+++ calculatrice-carbone/src/VisualisationProjet.svelte
@@ -0,0 +1,93 @@
+<script>
+	import TreeView from './TreeView.svelte'
+	let children = [
+		{
+			name: "Project",
+			type: "type",
+            parent : "",
+			children: [
+				{
+					name: "Build",
+					type: "type",
+                    parent : "Project",
+                    children: [
+						{
+							name: "Management",
+							type: "type",
+							parent: "Project/Build"
+						},
+						{
+							name: "Specifications",
+							type: "type",
+							parent: "Project/Build"
+						},
+                        {
+							name: "Implementation",
+							type: "type",
+							parent: "Project/Build"
+						}
+					]
+				},
+				{
+					name: "Run",
+					type: "type",
+					children: [
+						{
+							name: "Maintenance",
+							type: "type",
+							parent: "Project/Run"
+						},
+						{
+							name: "Hosting",
+							type: "type",
+							parent: "Project/Run"
+						}
+					]
+				}
+			]
+		}
+	]
+</script>
+
+<div class="container">
+
+    <div class="row h-100">
+
+
+      <div class="col">
+
+        <div class="row" style="padding-top : 20px;">
+            <div style="max-width : 200px;">
+                <input type="email" class="form-control" id="nameproject" placeholder="Name project">
+            </div>
+            <button type="button" class="btn btn-light" style="max-width : 200px; margin-right: 20px;">Compare</button>
+            <button type="button" class="btn btn-light" style="max-width : 200px;">Project</button>
+        </div>
+
+        <strong>Preview</strong>
+        
+        <div class="col">
+            <TreeView {children}></TreeView>
+        </div>
+
+      </div>
+
+      <div class="col-3 border-right h-100">
+          Impact by resource
+      </div>
+    </div>
+</div>
+
+<style>
+
+    .container{
+        max-width : 100%;
+        height: 90%;
+        max-height: 90%;
+    }
+
+    .border-right {
+        border-left: 1px solid #ddd;
+    }
+
+</style>
