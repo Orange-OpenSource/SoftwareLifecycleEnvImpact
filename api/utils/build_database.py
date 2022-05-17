2bf25c912710ca05439e05d718c2f445d7326621
Thibault Simon <thibault.simon@orange.com>
2022-05-17 11:15:38 +0200

    Added a real test project with the right resources and structure

--- api/utils/build_database.py
+++ api/utils/build_database.py
@@ -3,6 +3,7 @@ from api.data_model import db, Model, Project, Resource, Task
 projects = [
     {"id": 0, "name": "Project 0"},
     {"id": 1, "name": "Project 1"},
+    {"id": 2, "name": "Real project"}
 ]
 
 models = [
@@ -10,19 +11,55 @@ models = [
     {"id": 1, "name": "Model 1", "project_id": 0, "root_task_id": 0},
     {"id": 2, "name": "Model 2", "project_id": 1, "root_task_id": 0},
     {"id": 3, "name": "Model 3", "project_id": 1, "root_task_id": 0},
+    {"id": 4, "name": "Real project model 1", "project_id":2, "root_task_id": 3}
 ]
-
 
 tasks = [
     {"id": 0, "name": "Task 1", "task_type_id": 0, "model_id": 0},
     {"id": 1, "name": "Task 2", "parent_task_id": 0, "model_id": 0},
     {"id": 2, "name": "Task 3", "parent_task_id": 0, "model_id": 0},
+    {"id": 3, "name": "Real project", "model_id": 4},
+    {"id": 4, "name": "Build", "parent_task_id": 3, "model_id": 4},
+    {"id": 5, "name": "Implementation", "parent_task_id": 4, "model_id": 4},
+    {"id": 6, "name": "Development", "parent_task_id": 5, "model_id": 4},
+    {"id": 7, "name": "Design", "parent_task_id": 5, "model_id": 4},
+    {"id": 8, "name": "Specifications", "parent_task_id": 4, "model_id": 4},
+    {"id": 9, "name": "Management", "parent_task_id": 4, "model_id": 4},
+    {"id": 10, "name": "Run", "parent_task_id": 3, "model_id": 4},
+    {"id": 11, "name": "Maintenance", "parent_task_id": 10, "model_id": 4},
+    {"id": 12, "name": "Hosting", "parent_task_id": 10, "model_id": 4},
+    {"id": 13, "name": "Compute", "parent_task_id": 11, "model_id": 4},
+    {"id": 14, "name": "Storage", "parent_task_id": 11, "model_id": 4},
+    {"id": 15, "name": "Network", "parent_task_id": 11, "model_id": 4},
+    {"id": 16, "name": "Usage", "parent_task_id": 10, "model_id": 4},
 ]
+
+'''
+Real project
+    Build
+        Implementation
+            Development
+            Design
+        Specifications
+        Management
+    Run
+        Maintenance
+        Hosting
+        Usage
+'''
 
 resources = [
     {"id": 0, "name": "Resource 1", "task_id": 0, "type": "Compute", "value": 100},
     {"id": 1, "name": "Resource 2", "task_id": 1, "type": "People", "value": 100},
     {"id": 2, "name": "Resource 3", "task_id": 2, "type": "Storage", "value": 100},
+    {"id": 3, "name": "Development man days", "task_id": 6, "type": "People", "value": 100},
+    {"id": 4, "name": "Design man days", "task_id": 7, "type": "People", "value": 100},
+    {"id": 5, "name": "Specifications man days", "task_id": 8, "type": "People", "value": 100},
+    {"id": 6, "name": "Management man days", "task_id": 9, "type": "People", "value": 100},
+    {"id": 7, "name": "Servers amount", "task_id": 13, "type": "Compute", "value": 100},
+    {"id": 8, "name": "Storage terabytes", "task_id": 11, "type": "Storage", "value": 100},
+    {"id": 9, "name": "Data transferred", "task_id": 15, "type": "Network", "value": 100},
+    {"id": 10, "name": "Users", "task_id": 16, "type": "UserDevice", "value": 100},
 ]
 
 
