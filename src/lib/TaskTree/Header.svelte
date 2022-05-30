847ff84a4bbee15bfc0c018dc528de9fe27cd97e
Thibault Simon <thibault.simon@orange.com>
2022-05-30 09:49:09 +0200

    Removed rename model on top of tasktree

--- src/lib/TaskTree/Header.svelte
+++ src/lib/TaskTree/Header.svelte
@@ -29,13 +29,18 @@
 	<div>No model selected</div>
 {:else}
 	<div class="row" style="padding : 20px 0px 0px 10px; margin-bottom: 20px;">
-		<span class="col-4">
-			<input class="form-control" id="nameproject" placeholder="Name project" value={selectedModel.name} readonly={!modify} />
+		<span class="col">
+			<div class="card">
+				<h5 class="card-body">
+					{selectedModel.name}
+				</h5>
+			</div>
 		</span>
-
-		<div class="col-4 form-check form-switch" style="margin:5px 0px 0px 10px;">
-			<input on:click={changeState} class="form-check-input" type="checkbox" id="editmodeSwitch" checked={modify} />
-			<label class="form-check-label" style="width:100px;" for="editmodeSwitch">Editing mode</label>
+		<div class="col-8 form-check form-switch" style="margin:5px 0px 0px 10px;">
+			<div class="float-end">
+				<input on:click={changeState} class="form-check-input" type="checkbox" id="editmodeSwitch" checked={modify} />
+				<label class="form-check-label" style="width:100px;" for="editmodeSwitch">Editing mode</label>
+			</div>
 		</div>
 	</div>
 {/if}
