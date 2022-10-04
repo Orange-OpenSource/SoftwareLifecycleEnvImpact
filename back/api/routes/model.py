1d9390f6c0b6a34af2f16523ff97e619f086323f
Thibault Simon <thibault.simon@orange.com>
2022-10-04 14:22:09 +0200

    Added "copy" when duplicating model

--- back/api/routes/model.py
+++ back/api/routes/model.py
@@ -159,6 +159,7 @@ def duplicate_model(model_id: int) -> Any:
 
     if model is not None:
         model_copy = model.copy()
+        model_copy.name = model_copy.name + " copy"
         model_copy = insert_model_db(model_copy)
         model_schema = ModelSchema()
         return model_schema.dump(model_copy)
