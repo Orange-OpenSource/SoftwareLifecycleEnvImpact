84cef1cc87d739b37d888214d831bc37845d7769
tiboSmn <thibaultsimon378@gmail.com>
2022-12-28 11:18:54 +0100

    fix: d3js graph with empty data

--- front/src/lib/Dataviz/Sankey.svelte
+++ front/src/lib/Dataviz/Sankey.svelte
@@ -18,118 +18,120 @@
 	let sankeySVG: SVGSVGElement;
 
 	function drawSankey() {
-		// Create nodes
-		const linksSources = d3.map(links, (d: D3JSLink) => d.source);
-		const linksTargets = d3.map(links, (d: D3JSLink) => d.target);
+		if (links && links.length > 0) {
+			// Create nodes
+			const linksSources = d3.map(links, (d: D3JSLink) => d.source);
+			const linksTargets = d3.map(links, (d: D3JSLink) => d.target);
 
-		let nodes = Array.from(d3.union(linksSources, linksTargets), (id) => ({ id }));
-		const nodesNames = d3.map(nodes, (d) => d.id);
+			let nodes = Array.from(d3.union(linksSources, linksTargets), (id) => ({ id }));
+			const nodesNames = d3.map(nodes, (d) => d.id);
 
-		// Construct the color scale
-		const color = d3.scaleOrdinal(nodesNames, d3.schemeTableau10);
+			// Construct the color scale
+			const color = d3.scaleOrdinal(nodesNames, d3.schemeTableau10);
 
-		// Compute the Sankey layout.
-		d3Sankey
-			.sankey()
-			.nodeId((d) => d.id)
-			.nodeAlign(d3Sankey.sankeyJustify)
-			.nodeWidth(15)
-			.nodePadding(10) // vertical separation between adjacent nodes
-			.extent([
-				[marginLeft, marginTop],
-				[width - marginRight, height - marginBottom]
-			])({ nodes, links });
+			// Compute the Sankey layout.
+			d3Sankey
+				.sankey()
+				.nodeId((d) => d.id)
+				.nodeAlign(d3Sankey.sankeyJustify)
+				.nodeWidth(15)
+				.nodePadding(10) // vertical separation between adjacent nodes
+				.extent([
+					[marginLeft, marginTop],
+					[width - marginRight, height - marginBottom]
+				])({ nodes, links });
 
-		// Unique id to avoid link conflicts
-		const uid = `O-${Math.random().toString(16).slice(2)}`;
+			// Unique id to avoid link conflicts
+			const uid = `O-${Math.random().toString(16).slice(2)}`;
 
-		// Create SVG
-		const svg = select(sankeySVG);
+			// Create SVG
+			const svg = select(sankeySVG);
 
-		// Draw nodes
-		svg
-			.append('g')
-			.selectAll('rect')
-			.data(nodes)
-			.join('rect')
-			.attr('x', (d) => d.x0)
-			.attr('y', (d) => d.y0)
-			.attr('height', (d) => d.y1 - d.y0)
-			.attr('width', (d) => d.x1 - d.x0)
-			.attr('fill', (d) => color(d.id))
-			.on('mouseover', function (d) {
-				// Brighter node on hover
-				d3.select(this).attr('fill', (d) => d3.rgb(color(d.id)).brighter(0.8).toString());
-			})
-			.on('mouseout', function (d) {
-				// Reset node hover to default color
-				d3.select(this).attr('fill', (d) => color(d.id));
-			})
-			.append('title') // Node hover text
-			.text((d) => Math.round(d.value * 100) / 100 + ' kgCO2e');
+			// Draw nodes
+			svg
+				.append('g')
+				.selectAll('rect')
+				.data(nodes)
+				.join('rect')
+				.attr('x', (d) => d.x0)
+				.attr('y', (d) => d.y0)
+				.attr('height', (d) => d.y1 - d.y0)
+				.attr('width', (d) => d.x1 - d.x0)
+				.attr('fill', (d) => color(d.id))
+				.on('mouseover', function (d) {
+					// Brighter node on hover
+					d3.select(this).attr('fill', (d) => d3.rgb(color(d.id)).brighter(0.8).toString());
+				})
+				.on('mouseout', function (d) {
+					// Reset node hover to default color
+					d3.select(this).attr('fill', (d) => color(d.id));
+				})
+				.append('title') // Node hover text
+				.text((d) => Math.round(d.value * 100) / 100 + ' kgCO2e');
 
-		// Draw node labels
-		svg
-			.append('g')
-			.attr('font-family', 'sans-serif')
-			.attr('font-size', 10)
-			.selectAll('text')
-			.data(nodes)
-			.join('text')
-			.attr('x', (d) => (d.x0 < width / 2 ? d.x1 + 6 : d.x0 - 6))
-			.attr('y', (d) => (d.y1 + d.y0) / 2)
-			.attr('dy', '0.35em')
-			.attr('text-anchor', (d) => (d.x0 < width / 2 ? 'start' : 'end'))
-			.text((d) => d.id);
+			// Draw node labels
+			svg
+				.append('g')
+				.attr('font-family', 'sans-serif')
+				.attr('font-size', 10)
+				.selectAll('text')
+				.data(nodes)
+				.join('text')
+				.attr('x', (d) => (d.x0 < width / 2 ? d.x1 + 6 : d.x0 - 6))
+				.attr('y', (d) => (d.y1 + d.y0) / 2)
+				.attr('dy', '0.35em')
+				.attr('text-anchor', (d) => (d.x0 < width / 2 ? 'start' : 'end'))
+				.text((d) => d.id);
 
-		// Create link object
-		const link = svg
-			.append('g')
-			.attr('fill', 'none')
-			.attr('stroke-opacity', 0.5)
-			.selectAll('g')
-			.data(links)
-			.join('g')
-			.style('mix-blend-mode', 'multiply')
-			.on('mouseover', function (d) {
-				// Darken link on hover
-				d3.select(this).style('stroke-opacity', 1);
-			})
-			.on('mouseout', function (d) {
-				// Reset link hover to default color
-				d3.select(this).style('stroke-opacity', 0.5);
-			});
+			// Create link object
+			const link = svg
+				.append('g')
+				.attr('fill', 'none')
+				.attr('stroke-opacity', 0.5)
+				.selectAll('g')
+				.data(links)
+				.join('g')
+				.style('mix-blend-mode', 'multiply')
+				.on('mouseover', function (d) {
+					// Darken link on hover
+					d3.select(this).style('stroke-opacity', 1);
+				})
+				.on('mouseout', function (d) {
+					// Reset link hover to default color
+					d3.select(this).style('stroke-opacity', 0.5);
+				});
 
-		// Set links gradiant
-		link
-			.append('linearGradient')
-			.attr('id', (d) => `${uid}-link-${d.index}`)
-			.attr('gradientUnits', 'userSpaceOnUse')
-			.attr('x1', (d) => d.source.x1)
-			.attr('x2', (d) => d.target.x0)
-			.call(
-				(gradient) =>
-					gradient
-						.append('stop')
-						.attr('offset', '0%')
-						.attr('stop-color', (d) => color(d.source.id)) // Start gradient color
-			)
-			.call(
-				(gradient) =>
-					gradient
-						.append('stop')
-						.attr('offset', '100%')
-						.attr('stop-color', (d) => color(d.target.id)) // End gradient color
-			);
+			// Set links gradiant
+			link
+				.append('linearGradient')
+				.attr('id', (d) => `${uid}-link-${d.index}`)
+				.attr('gradientUnits', 'userSpaceOnUse')
+				.attr('x1', (d) => d.source.x1)
+				.attr('x2', (d) => d.target.x0)
+				.call(
+					(gradient) =>
+						gradient
+							.append('stop')
+							.attr('offset', '0%')
+							.attr('stop-color', (d) => color(d.source.id)) // Start gradient color
+				)
+				.call(
+					(gradient) =>
+						gradient
+							.append('stop')
+							.attr('offset', '100%')
+							.attr('stop-color', (d) => color(d.target.id)) // End gradient color
+				);
 
-		// Draw links
-		link
-			.append('path')
-			.attr('d', d3Sankey.sankeyLinkHorizontal())
-			.attr('stroke', ({ index: i }) => `url(#${uid}-link-${i})`)
-			.attr('stroke-width', (d) => Math.max(1, d.width))
-			.append('title') // Node hover text
-			.text((d) => Math.round(d.value * 100) / 100 + ' kgCO2e');
+			// Draw links
+			link
+				.append('path')
+				.attr('d', d3Sankey.sankeyLinkHorizontal())
+				.attr('stroke', ({ index: i }) => `url(#${uid}-link-${i})`)
+				.attr('stroke-width', (d) => Math.max(1, d.width))
+				.append('title') // Node hover text
+				.text((d) => Math.round(d.value * 100) / 100 + ' kgCO2e');
+		}
 	}
 
 	onMount(function () {
--- front/src/lib/Dataviz/StackedBarChart.svelte
+++ front/src/lib/Dataviz/StackedBarChart.svelte
@@ -16,97 +16,99 @@
 		marginLeft = 150;
 
 	function drawStackedBar() {
-		// Compute values
-		const X = d3.map(chartData, (d) => d.value);
-		const Y = d3.map(chartData, (d) => d.impactCategory);
-		const Z = d3.map(chartData, (d) => d.category);
+		if (chartData && chartData.length > 0) {
+			// Compute values
+			const X = d3.map(chartData, (d) => d.value);
+			const Y = d3.map(chartData, (d) => d.impactCategory);
+			const Z = d3.map(chartData, (d) => d.category);
 
-		// Compute default y- and z-domains, and unique them
-		let yDomain = new d3.InternSet(Y);
-		let zDomain = new d3.InternSet(Array.from(d3.union(Z))); // Unique category names
+			// Compute default y- and z-domains, and unique them
+			let yDomain = new d3.InternSet(Y);
+			let zDomain = new d3.InternSet(Array.from(d3.union(Z))); // Unique category names
 
-		// Create color scale
-		let colors = d3.schemeSpectral[zDomain.size];
+			// Create color scale
+			let colors = d3.schemeSpectral[zDomain.size];
 
-		// Omit any data not present in the y- and z-domains.
-		const I = d3.range(X.length).filter((i) => yDomain.has(Y[i]) && zDomain.has(Z[i]));
+			// Omit any data not present in the y- and z-domains.
+			const I = d3.range(X.length).filter((i) => yDomain.has(Y[i]) && zDomain.has(Z[i]));
 
-		// If the height is not specified, derive it from the y-domain.
-		height = yDomain.size * 25 + marginTop + marginBottom;
-		let yRange = [height - marginBottom, marginTop];
+			// If the height is not specified, derive it from the y-domain.
+			height = yDomain.size * 25 + marginTop + marginBottom;
+			let yRange = [height - marginBottom, marginTop];
 
-		// Compute a nested array of series where each series is [[x1, x2], [x1, x2],
-		// [x1, x2], …] representing the x-extent of each stacked rect. In addition,
-		// each tuple has an i (index) property so that we can refer back to the
-		// original data point (data[i]). This code assumes that there is only one
-		// data point for a given unique y- and z-value.
-		const series = d3
-			.stack()
-			.keys(zDomain)
-			.value(([, I], z) => X[I.get(z)])
-			.order(d3.stackOrderNone)
-			.offset(d3.stackOffsetExpand)(
-				d3.rollup(
-					I,
-					([i]) => i,
-					(i) => Y[i],
-					(i) => Z[i]
+			// Compute a nested array of series where each series is [[x1, x2], [x1, x2],
+			// [x1, x2], …] representing the x-extent of each stacked rect. In addition,
+			// each tuple has an i (index) property so that we can refer back to the
+			// original data point (data[i]). This code assumes that there is only one
+			// data point for a given unique y- and z-value.
+			const series = d3
+				.stack()
+				.keys(zDomain)
+				.value(([, I], z) => X[I.get(z)])
+				.order(d3.stackOrderNone)
+				.offset(d3.stackOffsetExpand)(
+					d3.rollup(
+						I,
+						([i]) => i,
+						(i) => Y[i],
+						(i) => Z[i]
+					)
 				)
-			)
-			.map((s) => s.map((d) => Object.assign(d, { i: d.data[1].get(s.key) })));
+				.map((s) => s.map((d) => Object.assign(d, { i: d.data[1].get(s.key) })));
 
-		// Compute the default y-domain. Note: diverging stacks can be negative.
-		let xDomain = d3.extent(series.flat(2));
+			// Compute the default y-domain. Note: diverging stacks can be negative.
+			let xDomain = d3.extent(series.flat(2));
 
-		// Construct scales, axes, and formats.
-		const xScale = d3.scaleLinear(xDomain, [marginLeft, width + marginLeft - marginRight]);
-		const yScale = d3.scaleBand(yDomain, yRange).paddingInner(0.2);
-		const color = d3.scaleOrdinal(zDomain, colors);
-		const xAxis = d3.axisTop(xScale).ticks(width / 80, '%');
-		const yAxis = d3.axisLeft(yScale).tickSizeOuter(0);
+			// Construct scales, axes, and formats.
+			const xScale = d3.scaleLinear(xDomain, [marginLeft, width + marginLeft - marginRight]);
+			const yScale = d3.scaleBand(yDomain, yRange).paddingInner(0.2);
+			const color = d3.scaleOrdinal(zDomain, colors);
+			const xAxis = d3.axisTop(xScale).ticks(width / 80, '%');
+			const yAxis = d3.axisLeft(yScale).tickSizeOuter(0);
 
-		// Create SVG
-		const svg = select(stackedBarChartSVG);
+			// Create SVG
+			const svg = select(stackedBarChartSVG);
 
-		// Create bars
-		const bar = svg
-			.append('g')
-			.selectAll('g')
-			.data(series)
-			.join('g')
-			.attr('fill', ([{ i }]) => color(Z[i]))
-			.selectAll('rect')
-			.data((d) => d)
-			.join('rect')
-			.attr('x', ([x1, x2]) => Math.min(xScale(x1), xScale(x2)))
-			.attr('y', ({ i }) => yScale(Y[i]))
-			.attr('width', ([x1, x2]) => Math.abs(xScale(x1) - xScale(x2)))
-			.attr('height', yScale.bandwidth())
-			.append('title') // Node hover text
-			.text(function (d) {
-				return Z[d.i] +': '+ X[d.i];
-			});
-		// .text((d) => Math.round(d.value * 100) / 100 + ' kgCO2e');
+			// Create bars
+			const bar = svg
+				.append('g')
+				.selectAll('g')
+				.data(series)
+				.join('g')
+				.attr('fill', ([{ i }]) => color(Z[i]))
+				.selectAll('rect')
+				.data((d) => d)
+				.join('rect')
+				.attr('x', ([x1, x2]) => Math.min(xScale(x1), xScale(x2)))
+				.attr('y', ({ i }) => yScale(Y[i]))
+				.attr('width', ([x1, x2]) => Math.abs(xScale(x1) - xScale(x2)))
+				.attr('height', yScale.bandwidth())
+				.append('title') // Node hover text
+				.text(function (d) {
+					return Z[d.i] + ': ' + X[d.i];
+				});
+			// .text((d) => Math.round(d.value * 100) / 100 + ' kgCO2e');
 
-		// Add upper legend
-		svg
-			.append('g')
-			.attr('transform', `translate(0,${marginTop})`)
-			.call(xAxis)
-			.call((g) => g.select('.domain').remove())
-			.call((g) =>
-				g
-					.append('text')
-					.attr('x', width - marginRight)
-					.attr('y', -22)
-					.attr('fill', 'currentColor')
-			);
+			// Add upper legend
+			svg
+				.append('g')
+				.attr('transform', `translate(0,${marginTop})`)
+				.call(xAxis)
+				.call((g) => g.select('.domain').remove())
+				.call((g) =>
+					g
+						.append('text')
+						.attr('x', width - marginRight)
+						.attr('y', -22)
+						.attr('fill', 'currentColor')
+				);
 
-		// Add left legend
-		svg
-			.append('g')
-			.attr('transform', `translate(${xScale(0)},0)`)
-			.call(yAxis);
+			// Add left legend
+			svg
+				.append('g')
+				.attr('transform', `translate(${xScale(0)},0)`)
+				.call(yAxis);
+		}
 	}
 
 	onMount(function () {
